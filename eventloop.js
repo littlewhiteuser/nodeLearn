dabd37622cc1ae79275b2fc76b919985721c9796
xuxiaohan <xuxiaohan@vandream.com>
2020-03-31 12:18:30 +0800

    0331

--- eventloop.js
+++ eventloop.js
@@ -1,5 +1,6 @@
 // console.log(process.nextTick) // node中实现的微任务，优先级比promise还要高
 // node的事件环，在node v10版本后，统一执行效果和浏览器一致
+// 每个宏任务执行完毕都会清空微任务
 process.nextTick(() => {
     console.log(1)
     process.nextTick(() => {
@@ -14,5 +15,9 @@ Promise.resolve().then(data => {
 })
 // nextTick和promise是两个不同的队列，nextTick的优先级更高，会先清空nextTick队列
 // 1 2 3 promise
+
+setImmediate(() => {
+  console.log('setImmediate') // node中的宏任务
+})
 
 // Buffer 是node中为了实现二进制操作，提供的类
--- test.js
+++ test.js
@@ -1,16 +1,36 @@
-let length = 10
-function fn() {
-    console.log(this.length)
-}
-let obj = {
-    length: 5,
-    method(fn){
-        fn()
-        arguments[0]()
-    }
-}
-obj.method(fn, 1)
+// let length = 10
+// function fn() {
+//     console.log(this.length)
+// }
+// let obj = {
+//     length: 5,
+//     method(fn){
+//         fn()
+//         arguments[0]()
+//     }
+// }
+// obj.method(fn, 1)
 
 // this.property length undefined
 
 // undefined 2
+
+function test() {
+    let arr = [3, 2, 1]
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i]
+        const res = fetch(item)
+        console.log(res)
+    }
+    console.log('end')
+}
+
+function fetch(x) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(x)
+        }, x * 500);
+    })
+}
+
+test()
